4bb3bfb35ede5417cad5c5fe09817d67e0872325
Quinten <104200472+K1ngQ1@users.noreply.github.com>
2022-09-02 15:27:04 +0200

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 //css import
 import "./App.css";
@@ -6,26 +6,71 @@ import "./App.css";
 //Custom component import
 import HeaderUnit from "./components/header";
 import HomePage from "./Pages/Home";
-import Guides from "./Pages/Guides"
-import Profile from "./Pages/Profile"
-import Coder from "./Pages/Coder"
+import Guides from "./Pages/Guides";
+import Profile from "./Pages/Profile";
+import Coder from "./Pages/Coder";
 
 //Component Import
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
+    const [theme, setTheme] = useState("dark");
+    const themes = [
+        "light",
+        "dark",
+        "cupcake",
+        "bumblebee",
+        "emerald",
+        "corporate",
+        "synthwave",
+        "retro",
+        "cyberpunk",
+        "valentine",
+        "halloween",
+        "garden",
+        "forest",
+        "aqua",
+        "lofi",
+        "pastel",
+        "fantasy",
+        "wireframe",
+        "black",
+        "luxury",
+        "dracula",
+        "cmyk",
+        "autumn",
+        "business",
+        "acid",
+        "lemonade",
+        "night",
+        "coffee",
+        "winter",
+    ];
     return (
         <Router>
-            <div className="App" data-theme="dark">
+            <div className="App" data-theme={theme}>
                 <header className="App-header">
                     <HeaderUnit />
 
                     <Switch>
-                        <Route path='/' exact component={HomePage}/>
-                        <Route path='/coder' exact component={Coder}/>
-                        <Route path='/guides' exact component={Guides}/>
-                        <Route path='/profile' exact component={Profile}/>
+                        <Route path="/" exact component={HomePage} />
+                        <Route path="/coder" exact component={Coder} />
+                        <Route path="/guides" exact component={Guides} />
+                        <Route path="/profile" exact component={Profile} />
                     </Switch>
+                    <select
+                        className="select absolute bottom-2 left-2"
+                        onChange={(e) => {
+                            setTheme(e.target.value);
+                        }}
+                    >
+                        <option disabled selected>
+                            Theme
+                        </option>
+                        {themes.map((theme) => (
+                            <option value={theme}>{theme}</option>
+                        ))}
+                    </select>
                 </header>
             </div>
         </Router>
